8e77bfaa0c2ab76572dea2560c0ed5947c91d07e
kumimochi <53399435+kumimochi@users.noreply.github.com>
2021-06-04 23:07:01 +0800

    functional now

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-const todoList = document.querySelector("ul");
-let input = document.querySelector("#input-field");
-let addTodo = document.querySelector("#input-btn");
+const input = document.querySelector('#input-field');
+const submitBtn = document.querySelector('#input-btn');
+const todoList = document.querySelector('.todo-list');
 
-addTodo.addEventListener("click", () => {
-  let lists = [];
-  const newTodo = document.createElement("li");
-  todoList.appendChild(newTodo);
-  newTodo.innerHTML =
-    input.value + '<span id="delete"><i class="fas fa-times"></i></span> ';
-  input.value = "";
-  lists.push(newTodo);
-  newTodo.addEventListener("click", () => {
-    newTodo.classList.toggle("completed");
+input.addEventListener('submit', addTodo);
+submitBtn.addEventListener('click', addTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+  if (input.value === '') {
+    return;
+  }
+  //create the todo li
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todoDiv');
+  const newTodo = document.createElement('li');
+  newTodo.innerText = input.value;
+  newTodo.classList.add('todo');
+  todoDiv.appendChild(newTodo);
+  console.log(todoDiv);
+  todoList.appendChild(todoDiv);
+  input.value = '';
+
+  //create delete button
+  const delBtn = document.createElement('button');
+  delBtn.classList.add('delete');
+  delBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+  todoDiv.appendChild(delBtn);
+
+  //add strikethrough text to completed tasks
+  newTodo.onclick = () => {
+    newTodo.classList.toggle('completed');
+  };
+
+  delBtn.addEventListener('click', (e) => {
+    todoDiv.remove();
   });
-});
+}
